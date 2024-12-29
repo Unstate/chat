5eedd26491a396eac73cd8ccf4b44eb55c4bf1ca
Fetisov <rofetisov@mail.ru>
2024-12-29 17:52:37 +0300

    #chore add eslint and prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import eslintPlugin from "@nabla/vite-plugin-eslint";
+import viteSvgr from 'vite-plugin-svgr'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(),eslintPlugin(), viteSvgr()],
     resolve: {
         alias: {
             "@app": "/src/app",
